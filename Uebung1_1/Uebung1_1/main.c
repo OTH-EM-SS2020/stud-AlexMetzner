d931e27c23f249b60d67d8b34d8572a52025952e
Taesd-Schdudaehnd <alexander.metzner@gmx.de>
2020-04-12 20:56:33 +0200

    Projekt anlegen

---
+++ Uebung1_1/Uebung1_1/main.c
@@ -0,0 +1,18 @@
+/*
+ * Uebung1_1.c
+ *
+ * Created: 12.04.2020 20:54:27
+ * Author : mea39511
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
